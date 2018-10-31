b14945f76ec098302d787223f591dc9155432de9
ONYPHE <40629718+onyphe@users.noreply.github.com>
2018-10-31 09:41:20 +0100

    Add files via upload

--- lib/Metabrik/Client/Onyphe.pm
+++ lib/Metabrik/Client/Onyphe.pm
@@ -1,1 +1,451 @@
+#
+# $Id: Onyphe.pm,v b753b43de9a5 2018/10/30 17:30:51 gomor $
+#
+package Metabrik::Client::Onyphe;
+use strict;
+use warnings;
 
+our $VERSION = '1.00';
+
+use base qw(Metabrik);
+
+sub brik_properties {
+   return {
+      revision => '$Revision: b753b43de9a5 $',
+      tags => [ qw(unstable) ],
+      author => 'ONYPHE <contact[at]onyphe.io>',
+      license => 'http://opensource.org/licenses/BSD-3-Clause',
+      attributes => {
+         apikey => [ qw(apikey) ],
+         apiurl => [ qw(apiurl) ],
+         autoscroll => [ qw(0|1) ],
+         _ao => [ qw(INTERNAL) ],
+         _sb => [ qw(INTERNAL) ],
+      },
+      attributes_default => {
+         apiurl => 'https://www.onyphe.io/api',
+         autoscroll => 0,
+      },
+      commands => {
+         simple => [ qw(api value page|OPTIONAL maxpage|OPTIONAL) ],
+         search => [ qw(query page|OPTIONAL maxpage|OPTIONAL) ],
+         output_dump => [ qw(results) ],
+         output => [ qw(
+            results fields|OPTIONAL encode|OPTIONAL separator|OPTIONAL cb|OPTIONAL
+         ) ],
+         output_csv => [ qw(results fields|OPTIONAL encode|OPTIONAL) ],
+         output_psv => [ qw(results fields|OPTIONAL encode|OPTIONAL) ],
+      },
+      require_modules => {
+         'Data::Dumper' => [ ],
+         'Metabrik::String::Base64' => [ ],
+         'Metabrik::Api::Onyphe' => [ ],
+      },
+   };
+}
+
+sub brik_init {
+   my $self = shift;
+
+   my $ao = Metabrik::Api::Onyphe->new_from_brik_init($self) or return;
+   $self->_ao($ao);
+
+   my $sb = Metabrik::String::Base64->new_from_brik_init($self) or return;
+   $self->_sb($sb);
+
+   return $self->SUPER::brik_init;
+}
+
+sub simple {
+   my $self = shift;
+   my ($api, $value, $page, $maxpage) = @_;
+
+   $page ||= 1;
+   my $apikey = $self->apikey;
+   $self->brik_help_set_undef_arg('apikey', $apikey) or return;
+   $self->brik_help_run_undef_arg('simple', $api) or return;
+
+   my $ao = $self->_ao;
+
+   if (! $ao->can($api)) {
+      return $self->log->error("simple: api [$api] unknown");
+   }
+
+   # Special case for user API
+   if ($api eq 'user') {
+      return $ao->user($apikey);
+   }
+
+   $self->brik_help_run_undef_arg('simple', $value) or return;
+
+   $self->log->verbose("simple: requesting page [$page]");
+
+   my @r = ();
+   my $results = $ao->$api($value, $apikey, $page) or return;
+   push @r, @$results;
+
+   $maxpage ||= $results->[0]{max_page};
+   $self->log->verbose("simple: maxpage is [$maxpage]");
+
+   $self->log->info("simple: page [$page/$maxpage] fetched");
+
+   if ($self->autoscroll && ++$page <= $maxpage) {
+      my $last = 0;
+      $SIG{INT} = sub {
+         $last = 1;
+         return \@r; 
+      };
+      for ($page..$maxpage) {
+         $self->log->verbose("simple: scrolling page [$_]");
+         my $results = $ao->$api($value, $apikey, $_) or return;
+         push @r, @$results;
+         my $perc = sprintf("%.02f%%", $_ / $maxpage * 100);
+         $self->log->info("simple: page [$_/$maxpage] fetched ($perc)...");
+         last if $last;
+      }
+   }
+
+   return \@r;
+}
+
+sub search {
+   my $self = shift;
+   my ($query, $page, $maxpage) = @_;
+
+   $page ||= 1;
+   my $apikey = $self->apikey;
+   $self->brik_help_set_undef_arg('apikey', $apikey) or return;
+   $self->brik_help_run_undef_arg('search', $query) or return;
+
+   my $ao = $self->_ao;
+
+   if ($query !~ m{^\s*category\s*:\s*(\w+)\s+(.+)\s*$}i) {
+      return $self->log->error("search: please start your search with ".
+         "'category:CATEGORY'");
+   }
+
+   my $category = lc($1);
+   my $search = $2;
+
+   my $api = 'search_'.$category;
+   if (! $ao->can($api)) {
+      return $self->log->error("search: category [$category] unknown");
+   }
+
+   $self->log->verbose("search: requesting page [$page]");
+
+   my @r = ();
+   my $results = $ao->$api($search, $apikey, $page) or return;
+   push @r, @$results;
+
+   $maxpage ||= $results->[0]{max_page};
+   $self->log->verbose("search: maxpage is [$maxpage]");
+
+   $self->log->info("search: page [$page/$maxpage] fetched");
+
+   if ($self->autoscroll && ++$page <= $maxpage) {
+      my $last = 0;
+      $SIG{INT} = sub {
+         $last = 1;
+         return \@r;
+      };
+      for ($page..$maxpage) {
+         $self->log->verbose("search: scrolling page [$_]");
+         my $results = $ao->$api($search, $apikey, $_) or return;
+         push @r, @$results;
+         my $perc = sprintf("%.02f%%", $_ / $maxpage * 100);
+         $self->log->info("search: page [$_/$maxpage] fetched ($perc)...");
+         last if $last;
+      }
+   }
+
+   return \@r;
+}
+
+sub _rec_header_from_result {
+   my $self = shift;
+   my ($r, $prev) = @_;
+
+   my %header = ();
+   for my $k (keys %$r) {
+      if (ref($r->{$k}) eq 'HASH') {
+         my $list = $self->_rec_header_from_result($r->{$k}, $k);
+         for (keys %$list) {
+            $header{"$k.$_"}++;
+         }
+      }
+      else {
+         $header{$k}++;
+      }
+   }
+
+   return \%header;
+}
+
+sub _build_header {
+   my $self = shift;
+   my ($results) = @_;
+
+   my %header = ();
+   for my $r (@$results) {
+      my $ary = $r->{results};
+      for my $this (@$ary) {
+         my $list = $self->_rec_header_from_result($this);
+         for (keys %$list) {
+            $header{$_}++;
+         }
+      }
+   }
+
+   #print "HEADER: ".Data::Dumper::Dumper(\%header)."\n";
+
+   return \%header;
+}
+
+sub output_dump {
+   my $self = shift;
+   my ($results) = @_;
+
+   $self->brik_help_run_undef_arg('output_dump', $results) or return;
+   $self->brik_help_run_invalid_arg('output_dump', $results, 'ARRAY') or return;
+
+   return Data::Dumper::Dumper($results);
+}
+
+sub output {
+   my $self = shift;
+   my ($results, $fields, $encode, $sep, $cb) = @_;
+
+   $sep ||= '|';
+   $fields ||= '';
+   $encode ||= '';
+   $self->brik_help_run_undef_arg('output', $results) or return;
+   $self->brik_help_run_invalid_arg('output', $results, 'ARRAY') or return;
+
+   my $sb = $self->_sb;
+
+   # Do nothing by default.
+   $cb ||= sub {
+      my ($line) = @_;
+      return $line;
+   };
+
+   my $summary = '';
+   my $header = $self->_build_header($results) or return;
+
+   my $string;
+   my $action;
+   my %fields = ();
+   if (length($fields)) {
+      ($action, $string) = $fields =~ m{^\s*((?:\-|\+))?(.+)\s*$};
+      $action ||= '+';  # Default to only keep fields given
+      %fields = map { $_ => 1 } split(/\s*,\s*/, $string);
+      for (keys %$header) {
+         if ($action eq '+') {
+            next if ($fields{$_});
+            delete $header->{$_};
+         }
+         else {
+            next if (! $fields{$_});
+            delete $header->{$_};
+         }
+      }
+   }
+
+   my %encode = ();
+   if (length($encode)) {
+      %encode = map { $_ => 1 } split(/\s*,\s*/, $encode);
+   }
+
+   my @header = sort { $a cmp $b } keys %$header;
+   if (@header == 0) {
+      return $self->log->error("output: no header found");
+   }
+
+   print $cb->(join($sep, @header))."\n";
+
+   for my $r (@$results) {
+      my $ary = $r->{results};
+      for my $this (@$ary) {
+         my $line = '';
+         for my $k (sort { $a cmp $b } keys %$header) {
+            if (keys %fields > 0) {
+               next if ($action eq '+' && ! $fields{$k});
+               next if ($action eq '-' && $fields{$k});
+            }
+
+            my @words = split(/\./, $k);
+            my $value = $this;
+
+            my $count = 0;
+            # Traverse the HASH to find the leaf value.
+            for (@words) {
+               last if (!exists($value->{$_}));
+               $value = $value->{$_};
+               $count++;
+            }
+            if ($count != @words) {  # It means there is no value for this key.
+               $line .= $sep;
+               next;
+            }
+
+            # Leaf value reached (maybe empty)
+            if (! defined($value)) {
+               $line .= $sep;
+               next;
+            }
+            elsif (ref($value) eq 'ARRAY') {
+               $line .= join(',', @$value).$sep;
+            }
+            else {
+               if (length($encode) && exists($encode{$k})) {
+                  $line .= $sb->encode($value).$sep;
+               }
+               else {
+                  $line .= $value.$sep;
+               }
+            }
+         }
+
+         $line =~ s{$sep$}{};
+         $line = $cb->($line);
+         print "$line\n" if length($line);
+      }
+   }
+
+   return $summary;
+}
+
+sub output_csv {
+   my $self = shift;
+   my ($results, $fields, $encode) = @_;
+
+   $fields ||= '';
+   $self->brik_help_run_undef_arg('output_csv', $results) or return;
+   $self->brik_help_run_invalid_arg('output_csv', $results, 'ARRAY') or return;
+
+   my $cb = sub {
+      my ($line) = @_;
+      $line =~ s{^}{"};
+      $line =~ s{$}{"};
+      return $line;
+   };
+
+   return $self->output($results, $fields, $encode, '","', $cb);
+}
+
+sub output_psv {
+   my $self = shift;
+   my ($results, $fields, $encode) = @_;
+
+   $fields ||= '';
+   $self->brik_help_run_undef_arg('output_psv', $results) or return;
+   $self->brik_help_run_invalid_arg('output_psv', $results, 'ARRAY') or return;
+
+   my $cb = sub {
+      my ($line) = @_;
+      $line =~ s{^\||\|$}{}g;
+      return $line;
+   };
+
+   return $self->output($results, $fields, $encode, '|', $cb);
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Metabrik::Client::Onyphe - official client for ONYPHE API access
+
+=head1 SYNOPSIS
+
+   use Metabrik::Client::Onyphe;
+
+   my $cli = Metabrik::Client::Onyphe->new_brik_init;
+   $cli->apikey(<APIKEY>);
+   $cli->autoscroll(<0|1>);
+
+   # Search first page of results only
+   my $results = $cli->search('category:datascan port:80');
+
+   # Search given page of results
+   my $results = $cli->search('category:datascan port:80', 10);
+
+   # Search from page 10 to page 20 when autoscroll is active
+   $cli->autoscroll(1);
+   my $results = $cli->search('category:datascan port:80', 10, 20);
+
+   # Fetch all pages when autoscroll is active
+   $cli->autoscroll(1);
+   my $results = $cli->search('category:datascan port:80');
+
+   # Search using the simple API
+   my $results = $cli->datascan('apache');
+   my $results = $cli->reverse('8.8.8.8');
+
+=head1 DESCRIPTION
+
+Official client for ONYPHE API access.
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item B<apikey>
+
+=item B<apiurl>
+
+=back
+
+=head1 COMMANDS
+
+=over 4
+
+=item B<brik_properties>
+
+=item B<brik_init>
+
+=item B<simple>
+
+Use simple API for queries.
+
+=item B<search>
+
+Use search API for queries.
+
+=item B<output_dump>
+
+Dump output using Perl's Data::Dumper
+
+=item B<output>
+
+Base command to other output modes.
+
+=item B<output_csv>
+
+Dump output in CSV format.
+
+=item B<output_psv>
+
+Dump output in PSV format.
+
+=back
+
+=head1 SEE ALSO
+
+L<Metabrik>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2018, ONYPHE
+
+You may distribute this module under the terms of The BSD 3-Clause License.
+See LICENSE file in the source distribution archive.
+
+=head1 AUTHOR
+
+ONYPHE
+
+=cut
