c330f94ed8360a3ac9fd4d622698bade7b527c61
GomoR <gomor@onyphe.io>
2021-10-26 17:23:45 +0200

    - UPDATE: function::whitelist: now fully working for CSV format

--- lib/Metabrik/Client/Onyphe/Function/Output.pm
+++ lib/Metabrik/Client/Onyphe/Function/Output.pm
@@ -19,20 +19,87 @@ sub brik_properties {
    };
 }
 
-# Not ready to be integrated, need to integrate -fields too and all onyphe program arguments.
-#sub run {
-#   my $self = shift;
-#   my ($page, $state, $format) = @_;
+sub run {
+   my $self = shift;
+   my ($page, $state, $args) = @_;
 
-#   $self->brik_help_run_undef_arg('run', $page) or return;
-#   $self->brik_help_run_undef_arg('run', $format) or return;
+   $self->brik_help_run_undef_arg('run', $page) or return;
+   $self->brik_help_run_undef_arg('run', $args) or return;
 
-#   my $function = 'output_'.$format;
-#   if (! $self->can($function)) {
-#      return $self->log->error("function_output: invalid output format [$format]");
-#   }
+   my $arg = $self->parse_v2($args);
+   #print Data::Dumper::Dumper($arg)."\n";
+   my $format = $arg->{format};
+   my $fields = $arg->{fields};
+   my $dedup = $arg->{dedup};
+   my $header = $arg->{header};
 
-#   return $self->$function($page);
-#}
+   $self->brik_help_run_undef_arg('run', $format) or return;
+
+   $format = $format->[0];  # Always single option
+
+   if ($format ne 'csv') {
+      return $self->log->error("function_output: invalid output format [$format]");
+   }
+
+   if (defined($fields)) {
+      $fields = { map { $_ => 1 } @$fields };
+   }
+
+   if (defined($dedup)) {
+      $dedup = { map { $_ => 1 } @$dedup };
+   }
+
+   my $cb = sub {
+      my ($this, $state, $new, $format, $fields, $dedup) = @_;
+
+      #print Data::Dumper::Dumper($this)."\n";
+
+      my @this_line = ();
+      for my $k (sort { $a cmp $b } keys %$this) {
+         # Only keep wanted fields when given, otherwise use all fields:
+         if (defined($fields)) {
+            next unless exists($fields->{$k});
+         }
+
+         # Make it always an ARRAY:
+         my $ary = (ref($this->{$k}) eq 'ARRAY') ? $this->{$k} : [ $this->{$k} ];
+
+         # Perform action against all elements of the ARRAY:
+         for my $e (@$ary) {
+            # Build dedup key when given:
+            my $key = '';
+            if (defined($dedup) && exists($dedup->{$k})) {
+               $key .= "$k:$e-";
+            }
+
+            # Skip when deduped:
+            #print "DEDUP[$key]\n";
+            next if $state->{output}{dedup}{$key};
+
+            push @this_line, $e;
+
+            # Update dedup cache:
+            if (length($key) && !exists($state->{output}{dedup}{$key})) {
+               $state->{output}{dedup}{$key}++
+            }
+         }
+      }
+
+      #print Data::Dumper::Dumper(\@this_line)."\n";
+
+      for (@this_line) {
+         if (defined($header) && !$state->{output}{header_print}) {
+            my $hdr = join('","', @$header);
+            print "\"$hdr\"\n";
+            $state->{output}{header_print}++;
+         }
+         print "\"$_\"\n";
+      }
+
+      return 1;
+   };
+
+   return $self->iter($page, $cb, $state, $format, $fields, $dedup);
+}
 
 1;
