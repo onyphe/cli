a008207444cc1b1b2b88aa452aee42004031aa1b
GomoR <gomor@onyphe.io>
2021-10-27 08:56:00 +0200

    - new: files

---
+++ lib/Metabrik/Client/Onyphe/Function/Expand.pm
@@ -0,0 +1,53 @@
+#
+# $Id$
+#
+package Metabrik::Client::Onyphe::Function::Expand;
+use strict;
+use warnings;
+
+use base qw(Metabrik::Client::Onyphe::Function);
+
+sub brik_properties {
+   return {
+      revision => '$Revision: d629ccff5f0c $',
+      tags => [ qw(client onyphe) ],
+      author => 'ONYPHE <contact[at]onyphe.io>',
+      license => 'http://opensource.org/licenses/BSD-3-Clause',
+      commands => {
+         run => [ qw(page state field) ],
+      },
+   };
+}
+
+sub run {
+   my $self = shift;
+   my ($page, $state, $field) = @_;
+
+   $self->brik_help_run_undef_arg('run', $page) or return;
+   $self->brik_help_run_undef_arg('run', $field) or return;
+
+   my $cb = sub {
+      my ($this, $state, $new, $field) = @_;
+
+      my $value = $self->value_as_array($this, $field) or return 1;
+
+      # Results are ordered in latest time first,
+      # thus we keep the freshest result.
+      my $keep = 0;
+      for my $v (@$value) {
+         if (! exists($state->{expand}{$v})) {
+            $state->{expand}{$v}++;
+            $keep = 1;
+         }
+      }
+      if ($keep) {
+         push @$new, $this;
+      }
+
+      return 1;
+   };
+
+   return $self->iter($page, $cb, $state, $field);
+}
+
+1;
---
+++ lib/Metabrik/Client/Onyphe/Function/Fields.pm
@@ -0,0 +1,53 @@
+#
+# $Id$
+#
+package Metabrik::Client::Onyphe::Function::Fields;
+use strict;
+use warnings;
+
+use base qw(Metabrik::Client::Onyphe::Function);
+
+sub brik_properties {
+   return {
+      revision => '$Revision: d629ccff5f0c $',
+      tags => [ qw(client onyphe) ],
+      author => 'ONYPHE <contact[at]onyphe.io>',
+      license => 'http://opensource.org/licenses/BSD-3-Clause',
+      commands => {
+         run => [ qw(page state args) ],
+      },
+   };
+}
+
+sub run {
+   my $self = shift;
+   my ($page, $state, $args) = @_;
+
+   $self->brik_help_run_undef_arg('run', $page) or return;
+   $self->brik_help_run_undef_arg('run', $args) or return;
+
+   my $arg = $self->parse($args);
+   my $keep = $arg->[0] || {};
+
+   if (defined($keep)) {
+      $keep = { map { $_ => 1 } split(/\s*,\s*/, $keep) };
+   }
+
+   my $cb = sub {
+      my ($this, $state, $new, $keep) = @_;
+
+      my %new = ();
+      for my $k (keys %$this) {
+         next if $keep->{$k};
+         delete $this->{$k};
+      }
+
+      @$new = [ $this ];
+
+      return 1;
+   };
+
+   return $self->iter($page, $cb, $state, $keep);
+}
+
+1;
