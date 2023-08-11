f5be1754971b99363ab201278751d22a8d2646dd
GomoR <gomor@onyphe.io>
2023-08-11 12:46:16 +0200

    - update: install.sh: create .onyphe.ini with user API key

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+APIKEY=$1
+
+if [[ -z $APIKEY ]]
+then
+   echo 'ERROR: please provide your API key as a parameter like:'
+   echo './install.sh MY_API_KEY'
+   exit 1
+fi
+
 if [[ -f "/etc/debian_version" || -f "/etc/lsb-release" ]]
 then
    echo "*** Installing dependencies for Ubuntu/Debian/Kali..."
@@ -9,16 +18,35 @@ then
    sudo cpanm -n Config::INI::Tiny Text::ParseWords Net::IPv4Addr Regexp::IPv4
    echo "*** Installing dependencies for Ubuntu/Debian/Kali...done"
    echo ''
+   if [[ ! -f ~/.onyphe.ini ]]
+   then
+      echo "*** Creating .onyphe.ini squeleton..."
+      echo 'api_endpoint = https://www.onyphe.io/api/v2' > ~/.onyphe.ini
+      echo "api_key = $APIKEY" >> ~/.onyphe.ini
+      echo 'api_maxpage = 1000' >> ~/.onyphe.ini
+      echo "*** Creating .onyphe.ini squeleton...done"
+      echo ''
+   fi
    echo "*** Installing ONYPHE CLI Perl module..."
    perl Build.PL
    ./Build && ./Build test
    sudo ./Build install
    echo "*** Installing ONYPHE CLI Perl module...done"
    echo ''
-   echo '*** ONE MORE STEP: configure your ~/.onyphe.ini file as described in README file'
+   echo 'Try the following query to verify everything works:'
+   echo "onyphe -search 'category:datascan domain:example.com'"
 else
-   echo "*** System unknown, can't install:"
-   echo 'Please proceed with manual install as described in README file'
+   if [[ ! -f ~/.onyphe.ini ]]
+   then
+      echo "*** Creating .onyphe.ini squeleton..."
+      echo 'api_endpoint = https://www.onyphe.io/api/v2' > ~/.onyphe.ini
+      echo "api_key = $APIKEY" >> ~/.onyphe.ini
+      echo 'api_maxpage = 1000' >> ~/.onyphe.ini
+      echo "*** Creating .onyphe.ini squeleton...done"
+      echo ''
+   fi
+   echo "ERROR: System unknown, can't install automatically:"
+   echo 'Please proceed with manual install as described in README file paragraph 2.'
 fi
 
 exit 0
